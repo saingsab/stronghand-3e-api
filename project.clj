ee35cb952e4d27c26bd65da2907cb6f09d367a3a
Saing Sab <saing.sab@gmail.com>
2021-04-11 15:13:22 +0700

    init

---
+++ project.clj
@@ -0,0 +1,9 @@
+ (defproject stronghand-3e-api "0.1.0-SNAPSHOT"
+   :description "FIXME: write description"
+   :dependencies [[org.clojure/clojure "1.10.0"]
+                  [metosin/compojure-api "2.0.0-alpha30"]
+                  [metosin/ring-swagger-ui "3.20.1"]]
+   :ring {:handler stronghand-3e-api.handler/app}
+   :uberjar-name "server.jar"
+   :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]
+                   :plugins [[lein-ring "0.12.5"]]}})
---
+++ src/stronghand_3e_api/handler.clj
@@ -0,0 +1,38 @@
+(ns stronghand-3e-api.handler
+  (:require [compojure.api.sweet :refer :all]
+            [ring.util.http-response :refer :all]
+            [schema.core :as s]
+            [stronghand-3e-api.schema :as sc]))
+
+(s/defschema Pizza
+  {:name s/Str
+   (s/optional-key :description) s/Str
+   :size (s/enum :L :M :S)
+   :origin {:country (s/enum :FI :PO)
+            :city s/Str}})
+
+(def app
+  (api
+    {:swagger
+     {:ui "/"
+      :spec "/swagger.json"
+      :data {:info {:title "Stronghand-3e-api"
+                    :description "Compojure Api example"}
+             :tags [{:name "api", :description "some apis"}]}}}
+      sc/routes
+
+    ;; (context "/api" []
+    ;;   :tags ["api"]
+
+    ;;   (GET "/plus" []
+    ;;     :return {:result Long}
+    ;;     :query-params [x :- Long, y :- Long]
+    ;;     :summary "adds two numbers together"
+    ;;     (ok {:result (+ x y)}))
+
+    ;;   (POST "/echo" []
+    ;;     :return Pizza
+    ;;     :body [pizza Pizza]
+    ;;     :summary "echoes a Pizza"
+    ;;     (ok pizza)))
+        ))
