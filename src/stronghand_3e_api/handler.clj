d0ae502880d169eccb49079349b8e50220e8bccb
Saing Sab <saing.sab@gmail.com>
2021-04-14 14:21:58 +0700

    add dec

--- src/stronghand_3e_api/handler.clj
+++ src/stronghand_3e_api/handler.clj
@@ -8,8 +8,9 @@
    {:swagger
     {:ui "/"
      :spec "/swagger.json"
-     :data {:info {:title "Stronghand-3e-api"
-                   :description "Compojure Api example"}
-            ;;:tags [{:name "api", :description "some apis"}]
-            }}}
+     :data {:info {:title "STRONGHAND 3E API"
+                   :description "Welcome to the STRONGHAND 3E API , This APIs are a set of endpoints created to manage customer order and matching in our platform."
+                   :contact {:name "Officail Website"
+                             :email "saing.sab@gmail.com"
+                             :url "https://www.stronghand3e.com"}}}}}
    sc/routes))
